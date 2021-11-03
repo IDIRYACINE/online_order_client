5953d2bfd8a4a951db0b12f991fadc4410399a46
wawadz2000 <wawadz2000@gmail.com>
2021-11-03 18:35:38 +0100

    No error XD

--- lib/Ui/shared/Components.dart
+++ lib/Ui/shared/Components.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:online_order_client/Models/catalogue_model.dart';
+CatalogueModel catalogue= new CatalogueModel();
 Widget defaultbutton ({
    Color? color=Colors.red,
    double? width=double.infinity,
    double? hight=70,
    double? raduis=0,
-  required double? CategorieID, 
+  required int CategorieID, 
   required Function? function}){
     return Container(
             decoration: BoxDecoration(
@@ -16,7 +17,7 @@ Widget defaultbutton ({
             height:hight,
             child: MaterialButton(
               onPressed:(){ function; },
-              child: Text('${getCategoryName(CategorieID)}',
+              child: Text('${catalogue.getCategoryName(categoryId: CategorieID)}',
               style: const TextStyle(
                 fontSize: 40,
                 fontFamily: 'Dancing',
