697d7e632d4a98d80322c9fdbef650f996b9bb70
IDIRYACINE <idiryacinesp@gmail.com>
2022-03-17 13:21:13 +0100

    updated dependencies

--- lib/Ui/Profile/Profile_dialoge.dart
+++ lib/Ui/Profile/Profile_dialoge.dart
@@ -11,49 +11,49 @@ class ChangeElementProfile extends StatefulWidget {
 class _ChangeElementProfileState extends State<ChangeElementProfile> {
   @override
   Widget build(BuildContext context) {
-       return  Form(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Container(
-                    height: 150,
-                    child: Column(
-                              children: [
-                     SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          hintText: "current Email",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          prefixIcon: Icon(
-                            Icons.mail,
-                            color: parseColor("#FFB5A7"),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          hintText: "new adresse Email",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          prefixIcon: Icon(
-                            Icons.mail,
-                            color: parseColor("#FFB5A7"),
-                          ),
-                        ),
-                      ),
-                    ),
-                              ],
-                            ),
+    return Form(
+        child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        height: 150,
+        child: Column(
+          children: [
+            SizedBox(
+              width: 220,
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "current Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: parseColor("#FFB5A7"),
                   ),
-                )); 
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 220,
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "new adresse Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: parseColor("#FFB5A7"),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
